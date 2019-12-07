c4af94fcb7c3052cb1afcecd83ef44cd53492607
Gernene <wallie988@gmail.com>
2019-12-07 14:35:50 -0700

    Allow slot select toggle

--- client/src/components/SlotSelector.jsx
+++ client/src/components/SlotSelector.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { Slot } from "./Slot";
 
 export function SlotSelector () {
   const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+  // 2D boolean array representing whether slot is selected
+  const initSlots = [];
+  for (let i=0; i<5; i++) {
+    const daySlots = []; 
+    for (let j=0; j<24; j++) {
+      daySlots.push(false);
+    }
+    initSlots.push(daySlots);
+  }
+
+  const [slots, setSlots] = useState(initSlots);
+  const toggleSlot = (day, num) => {
+    slots[day][num] = !slots[day][num];
+    setSlots([...slots]);
+  };
+
   return (
     <div className="slot-selector">
       <div className="slot-selector-days">
@@ -11,15 +29,15 @@ export function SlotSelector () {
       </div>
       <div className="slot-selector-schedules">
         <div className="slot-selector-times">
-          {[...Array(12)].map((e, i) => {
+          {slots[0].map((slot, i) => {
               return (<div>{i+7}:00</div>);
           })}
         </div>
-        {days.map(day => {
+        {days.map((day, i) => {
           return (
             <div className="slot-selector-schedule">
-              {[...Array(24)].map((e, i) => {
-                return <div className="slot-selector-slot" key={i}/>
+              {slots[i].map((slot, j) => {
+                return <Slot toggleSlot={toggleSlot} isSelected={slot} day={i} num={j} key={j}/>
               })}
             </div>
           );
