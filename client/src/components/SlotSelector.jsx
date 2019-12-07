d9b448188c1b3178bc427fd0650975689944e77f
Gernene <wallie988@gmail.com>
2019-12-07 13:18:28 -0700

    Add starting styles for slot selector

--- client/src/components/SlotSelector.jsx
+++ client/src/components/SlotSelector.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 
 export function SlotSelector () {
+  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
   return (
-    <>
-      Books!
-    </>
+    <div className="slot-selector">
+      <div className="slot-selector-days">
+        {days.map(day => {
+          return (<div>{day}</div>);
+        })}
+      </div>
+      <div className="slot-selector-schedules">
+        <div className="slot-selector-times">
+          {[...Array(12)].map((e, i) => {
+              return (<div>{i+7}:00</div>);
+          })}
+        </div>
+        {days.map(day => {
+          return (
+            <div className="slot-selector-schedule">
+              {[...Array(24)].map((e, i) => {
+                return <div className="slot-selector-slot" key={i}/>
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
